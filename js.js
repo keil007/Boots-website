80d7a1641578db8c0ad70c9a971562e76df83c1f
keil007 <ejmont.m@gmail.com>
2020-11-20 08:13:23 +0100

    Modificated structure a little

---
+++ js.js
@@ -0,0 +1,9 @@
+/*$('#welcome_button').on('click', function() {
+    const toptop = $('#content').position().top;
+
+    $('html, body').animate(
+      {
+        scrollTop: toptop
+      },
+      1100);
+});
